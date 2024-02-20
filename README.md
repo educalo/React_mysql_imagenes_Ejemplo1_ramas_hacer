391933eca055ebe52d9fe31dbe696a93b13c3e2e
educalo <64586573+educalo@users.noreply.github.com>
2024-02-20 14:44:45 +0100

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-Tratamiento de imagenes en React. Esta primera rama permite subir imagenes a una base de datos.
+Tratamiento de imagenes en React. Esta segunda rama permite subir y mostrar imagenes de una base de datos.
--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,38 @@
 //voy a utilizar un fragment para añadir varios etiquetas en mi return del componente App
-import React, {Fragment, useState} from 'react';
+import React, {Fragment, useState, useEffect} from 'react';
+//para manejar formularios modales
+import Modal from 'react-modal'
 
 function App() {
 
   const [file, setFile] = useState(null)
+  //lista del nombre de las imagenes que estan en el banco de imagenes y estan siendo servidas por el servidor
+  const [imageList, setImageList] = useState([])
+
+  //estado para conseguir que una imagen cargada se muestre directamente en la parte cliente
+  const [listUpodated, setListUpdate] = useState(false)
+
+  //estado para manejar el formulario modal
+  const [modalIsOpen, setModalIsOpen] = useState(false)
+
+  //estado de la imagen para abrir en el modal
+  const [currentImage, setCurrentImage] = useState(null)
+
+  //para mostar las imagenes del estado imageList
+  useEffect (()=>{
+
+    //lo voy asociar al body del documento html
+    Modal.setAppElement('body')
+
+    fetch('http://localhost:3000/images/get')
+    .then(res => res.json())
+    .then(res => setImageList(res))
+    .catch(err => {
+      console.error(err)
+    })  
+    setListUpdate(false)
+  },[listUpodated])
+
 
   //array con todos los archivos seleccionados, en nuestro caso solo uno, el primero
   const selectedHandler = e => {
@@ -23,12 +52,16 @@ function App() {
     const formdata = new FormData()
     formdata.append('image', file)
 
-    fetch('http://localhost:9000/images/post', {
+    fetch('http://localhost:3000/images/post', {
       method: 'POST',
       body: formdata
     })
     .then(res => res.text())
-    .then(res => console.log(res))
+    .then(res => {
+        console.log(res)
+        //como el estado esta cambiado realiza de nuevo el useffect
+        setListUpdate(true)
+    })
     .catch(err => {
       console.error(err)
     })
@@ -38,9 +71,25 @@ function App() {
     setFile(null)
   }
 
+  const modalHandler = (isOpen, image) => {
+    setModalIsOpen(isOpen)
+    setCurrentImage(image)
+  }
+
+  const deleteHandler = () =>{
+    
+    let imageID = currentImage.split('-')
+    //para comprobar si tengo el numero del id en la descripcion de la imagen
+    console.log(imageID[0])
+    
+    //fetch('http://localhost:3000/images/delete')
+  }
+
   //nav es un navegador
   //href="#!" no apunta a ningun lado
   //input de tipo file para cargar un archivo
+  //haga una rejilla de datos y no lo amontone en una misma fila, sino utilice mas de una
+  //onRequestClose se va a activar cuando hagamos click fuera del modal
   return (
     <Fragment>
       <nav className="navbar navbar-dark bg-dark">
@@ -61,6 +110,28 @@ function App() {
           </div>
         </div>
       </div>
+
+      <div className="container mt-3" style={{display:"flex", flexWrap: "wrap"}}>
+
+        {imageList.map(image => (
+          <div key={image} className="card m-2">
+          <img src={'http://localhost:3000/'+ image} alt="..." className="card-img-top" styte={{height: "200px", with: "300px"}}/>
+          <div classNmae="card-body">
+            <button onClick={()=> modalHandler(true, image)} className='btn btn-dark'>Click para ver</button>
+          </div>
+        </div>
+
+        ))}
+      </div>
+
+          <Modal style={{content:{right: "20%", left: "20%"}}} isOpen={modalIsOpen} onRequestClose={()=>modalHandler(false, null)}>
+              <div className="card"></div>
+              <img src={'http://localhost:3000/'+ currentImage}  alt="..."/>
+                <div className='card-body'>
+                  <button onClick={() => deleteHandler()}       className='btn btn-danger'>Delete</button>
+                </div>
+          </Modal>
+
     </Fragment>
   );
 }
