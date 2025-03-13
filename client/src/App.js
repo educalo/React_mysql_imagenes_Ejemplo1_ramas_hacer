c17fabc08f09432acb8502aab42b421af9f08209
educalo <64586573+educalo@users.noreply.github.com>
2025-03-13 23:24:27 +0100

    Add files via upload

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,5 @@
 //voy a utilizar un fragment para añadir varios etiquetas en mi return del componente App
+//fragment nos permite exportar varios elementos de HTML
 import React, {Fragment, useState} from 'react';
 
 function App() {
@@ -7,12 +8,13 @@ function App() {
 
   //array con todos los archivos seleccionados, en nuestro caso solo uno, el primero
   const selectedHandler = e => {
+    //para mostrar las propiedades de la imagen seleccionada
     //console.log(e.target.file[0])
     //para colocarlo en el estado de mi componente
     setFile(e.target.files[0])
   }
 
-  //funcion para el envio del fichero seleccionado
+  //funcion para el envio del fichero seleccionado al servidor en la carpeta imagenes
   const sendHandler = () => {
     if(!file){
       alert('you must upload file')
@@ -21,8 +23,11 @@ function App() {
 
     //clase de js para formatear un archivo (estado del componente) en nuestro caso de imagen
     const formdata = new FormData()
+    //agrego un elemento que lo voy a llamar image y el archivo a enviar file
     formdata.append('image', file)
 
+    //se podria hacer con axios tambien
+    //paso la direccion y luego un objeto
     fetch('http://localhost:9000/images/post', {
       method: 'POST',
       body: formdata
